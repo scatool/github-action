0865a7a3ae240877286f2b3dcd8b351b0ab468b6
Emanuel Erben <emanuel.erben@gmail.com>
2025-01-31 16:46:45 +0100

    add check uploads function, that will check the files that are found before they are uploaded to prevent uploads that cant work from the beginning.

---
+++ GitHub/Action/src/check_upload.ts
@@ -0,0 +1,40 @@
+import * as core from '@actions/core';
+
+/**A typescript funtion that gets a filelist and a list of list of filetypes. 
+* It should check if at least one file is availbale to be uploaded and if the combinations of at least on filetype list is completly represented in the files. 
+* If not it should let the action fail and inform the user what files are searched for.
+* This is important to prevent uploads that cant work because of missing files.
+* @param {string[]} fileList - The list of files to be uploaded.
+* @param {string[][]} fileTypes - The list of file types to be uploaded.
+* @returns {void} - No return value.
+*/
+
+function checkUpload(fileList: string[], fileTypes: string[][]): void {
+  // Check if the file list is empty
+  if (fileList.length === 0) {
+    core.setFailed('No files found to upload.');
+    process.exit();
+  }
+
+  // Check if the file types list is empty
+  if (fileTypes.length === 0) {
+    core.setFailed('Internal Error. Please check for update of action or contact us.');
+    process.exit();
+  }
+
+  // Check if the combinations of at least one filetype list is completely represented in the files
+  let check_passed = fileTypes.some((fileTypeCombi) => 
+    fileTypeCombi.every((fileType) => 
+        fileList.some((file) => file.endsWith(fileType))
+    )
+  );
+
+    if (!check_passed) {
+        core.setFailed(`Not all necessary files are presen in the selected scope. Please ensure at least one combination of following types is included in the configured paths: \n${fileTypes.map(pair => `[ ${pair.join(", ")} ]`).join("\n")}`);
+        process.exit();
+    }
+
+  core.info('All checks passed.');
+}
+
+export default checkUpload;
